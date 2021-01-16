d105e62d4c162b30618055fc1f7b1fc3fb72ed7c
남쭌 <jjune095@naver.com>
2021-01-17 00:56:57 +0900

    SWEA 2001 D2 java

---
+++ java/SWEA/2001.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int[][] map = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = sc.nextInt();
+                }
+            }
+            int max = 0;
+            for (int i = 0; i < N - M + 1; i++) {
+                for (int j = 0; j < N - M + 1; j++) {
+                    int sum = 0;
+                    for (int k = 0; k < M; k++) {
+                        for (int p = 0; p < M; p++) {
+                            sum += map[i + k][j + p];
+                        }
+                    }
+                    if (max < sum)
+                        max = sum;
+                }
+            }
+            System.out.println("#" + t + " " + max);
+        }
+    }
+}
+
