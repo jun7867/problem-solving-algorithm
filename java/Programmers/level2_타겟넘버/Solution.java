93fbdbf09c904bf2e15b7dc6d53de165fbb7c6e0
jun7867 <jjune095@naver.com>
2021-06-26 13:33:24 +0900

    Java Level2 타겟넘버 dfs

---
+++ java/Programmers/level2_타겟넘버/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int cnt=0;
+    public int solution(int[] numbers, int target) {
+       
+        dfs(0,0,numbers,target);
+        return cnt;
+    }
+    
+    private static void dfs(int idx,int sum,int[] numbers, int target){
+        if(idx==numbers.length){
+            if(sum==target){
+                cnt++;
+            }
+            return;
+        }
+        
+        dfs(idx+1,sum+numbers[idx],numbers,target);
+        dfs(idx+1,sum-numbers[idx],numbers,target);
+    }
+}
