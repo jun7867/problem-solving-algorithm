afd1994bc6eaf8b55728423ef98df25ae1e49b98
Jun-yeong Nam <36908476+jun7867@users.noreply.github.com>
2022-06-18 21:43:37 +0900

    Update leetCode_9.js

--- javascript/leetCode_9.js
+++ javascript/leetCode_9.js
@@ -2,6 +2,9 @@
  * @param {number} x
  * @return {boolean}
  */
+
+// 9. Palindrome Number
+// https://leetcode.com/problems/palindrome-number/
 var isPalindrome = function(x) {
     if (x < 0) return false;
     const xArray = x.toString().split('');
